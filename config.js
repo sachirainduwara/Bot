c761d451a24fa3ef9d155589f48f516865b9cb7f
sachirainduwara <sachirainduwara02@gmail.com>
2026-08-08 14:06:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,10 @@
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
-function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
-}
-
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "",
-    PREFIX: process.env.PREFIX || ".",
-    OWNER_NUM: process.env.OWNER_NUM || "94771081150",
-    ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/sachirainduwara/Bot/blob/main/images/SACHIYA%20MD.png?raw=true",
-    MEGA_EMAIL: process.env.MEGA_EMAIL || "sachirainduwara02@gmail.com",
-    MEGA_PASSWORD: process.env.MEGA_PASSWORD || "Sachi@2010"
+  SESSION_ID: process.env.SESSION_ID || "mongodb+srv://sachirainduwara02_db_user:ඔයාගේ_පාස්වර්ඩ්_එක@cluster0.skykj4x.mongodb.net/?appName=Cluster0",
+  PREFIX: process.env.PREFIX || ".",
+  OWNER_NUM: process.env.OWNER_NUM || "94771081150",
+  ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/sachirainduwara/Bot/blob/main/images/SACHIYA%20MD.png?raw=true",
+  MODE: process.env.MODE || "public"
 };
