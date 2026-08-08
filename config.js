2fa03b449c5bd200db938620aa3c194f3ea40a3b
sachirainduwara <sachirainduwara02@gmail.com>
2026-08-08 12:04:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,5 +9,7 @@ module.exports = {
     SESSION_ID: process.env.SESSION_ID || "",
     PREFIX: process.env.PREFIX || ".",
     OWNER_NUM: process.env.OWNER_NUM || "94760579211",
-    ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/sachirainduwara/Bot/blob/main/images/SACHIYA%20MD.png?raw=true"
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/sachirainduwara/Bot/blob/main/images/SACHIYA%20MD.png?raw=true",
+    MEGA_EMAIL: process.env.MEGA_EMAIL || "sachirainduwara02@gmail.com",
+    MEGA_PASSWORD: process.env.MEGA_PASSWORD || "Sachi@2010"
 };
