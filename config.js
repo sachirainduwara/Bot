d81d9cf1a0a10953a3b5b50873d94c985a9d7ff6
sachirainduwara <sachirainduwara02@gmail.com>
2026-08-08 13:37:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
     SESSION_ID: process.env.SESSION_ID || "",
     PREFIX: process.env.PREFIX || ".",
-    OWNER_NUM: process.env.OWNER_NUM || "94760579211",
+    OWNER_NUM: process.env.OWNER_NUM || "94771081150",
     ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/sachirainduwara/Bot/blob/main/images/SACHIYA%20MD.png?raw=true",
     MEGA_EMAIL: process.env.MEGA_EMAIL || "sachirainduwara02@gmail.com",
     MEGA_PASSWORD: process.env.MEGA_PASSWORD || "Sachi@2010"
