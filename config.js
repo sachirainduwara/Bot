455926810efcd7185ed892e076ca1e0885d732dd
sachirainduwara <sachirainduwara02@gmail.com>
2026-08-08 14:21:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 module.exports = {
   SESSION_ID: process.env.SESSION_ID || "mongodb+srv://sachirainduwara02_db_user:ඔයාගේ_පාස්වර්ඩ්_එක@cluster0.skykj4x.mongodb.net/?appName=Cluster0",
   PREFIX: process.env.PREFIX || ".",
-  OWNER_NUM: process.env.OWNER_NUM || "94771081150",
+  OWNER_NUM: process.env.OWNER_NUM || "94760579211",
   ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/sachirainduwara/Bot/blob/main/images/SACHIYA%20MD.png?raw=true",
   MODE: process.env.MODE || "public"
 };
