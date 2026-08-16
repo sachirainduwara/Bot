7c3cbf7d2b81884ea86fbb06152b8298f371df28
sachirainduwara <sachirainduwara02@gmail.com>
2026-08-16 13:27:56 +0530

    Update song.js

--- plugins/song.js
+++ plugins/song.js
@@ -30,7 +30,7 @@ cmd({
 
         await conn.sendMessage(from, { image: { url: data.thumbnail }, caption: desc }, { quoted: mek });
 
-        let stream = ytdl(data.url, { filter: 'audioonly', quality: highestaudio });
+        let stream = ytdl(data.url, { filter: 'audioonly', quality: 'highestaudio' });
 
         await conn.sendMessage(from, { 
             audio: stream, 
