7e740d3430a3b4903b88566f6c17df5509073f52
sachirainduwara <sachirainduwara02@gmail.com>
2026-08-19 08:08:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -226,7 +226,7 @@ async function connectToWA() {
       creds: state.creds,
       keys: makeCacheableSignalKeyStore(state.keys, logger),
     },
-    syncFullHistory: false,
+    syncFullHistory: false, // ⚡ බොට් ස්ටාර්ට් වෙද්දි history sync වෙලා හිරවීම වැළැක්වීමට
     fireInitQueries: true,
     markOnlineOnConnect: true,
     generateHighQualityLinkPreview: false,
@@ -262,7 +262,6 @@ async function connectToWA() {
     const { connection, lastDisconnect } = update;
     
     if (connection === 'close') {
-      isConnectedOnce = false;
       const statusCode = lastDisconnect?.error?.output?.statusCode;
       
       if (statusCode === DisconnectReason.loggedOut) {
